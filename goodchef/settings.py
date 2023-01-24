b0084dadfe241b4016559f30b7113426b30ea6e9
KarlP17123 <karl.park@telia.com>
2023-01-24 16:47:38 +0000

    changed ALLOWED_HOST name

--- goodchef/settings.py
+++ goodchef/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chef2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['chef.herokuapp.com', 'localhost']
 
 
 # Application definition
